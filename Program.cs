78c77d3a0a62ca5188fe9a4cefb31ca765ce2eb1
minelsulun <118488899+minelsulun@users.noreply.github.com>
2024-04-16 23:24:30 +0300

    Add files via upload
    NumberGuesser
    This simple C# console application allows the user to guess a randomly generated number between 1 and 10.
    
    How to Run
    Prerequisites
    
    You need to have .NET Framework or .NET Core installed to run this program.
    Running the Application
    
    Compile and run the Program.cs file in the project's root directory.
    Upon starting the application, the user will be prompted to enter their name and whether they want to play a game.
    If the user answers "yes," they will be asked to guess a number between 1 and 10.
    The program will terminate when the user correctly guesses the number.
    If the user answers "no," the program will exit without starting the game.
    Additional Information
    This project is written in C#.
    Proper error handling is implemented for user inputs and invalid scenarios.
    The Random class is used to generate random numbers.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello {0} do you want to play game?(yes or no)",name);
+            string response = Console.ReadLine();
+            if (response.ToLower()=="yes")
+            {
+                Console.WriteLine("Lets go then!!");
+                PlayGame(); // start game
+            }
+            else if (response.ToLower() == "no")
+            {
+                Console.WriteLine("Too bad. Maybe another time!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid response. Please respond with 'yes' or 'no'.");
+            }
+            Console.ReadKey();
+
+            void PlayGame()
+            {
+                //creat a new random object
+                Random random = new Random();
+                int correctNumber = random.Next(1,10);
+                int guess = 0;
+
+                Console.WriteLine("guess a number between 1-10");
+
+                while (guess != correctNumber)
+                {
+                    //get users input
+                    string input = Console.ReadLine();
+                    if(!int.TryParse(input, out guess))
+                    {
+                        Console.WriteLine("enter an number");
+                        continue;
+                    }
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+                        guess = 0;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("wrong number, correct number was {0}- another number created try again?", correctNumber);
+                        correctNumber = random.Next(1, 10);
+                        
+                        Console.ResetColor();
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("CORRECT number was {0}!!",correctNumber);
+
+                Console.ResetColor();
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
